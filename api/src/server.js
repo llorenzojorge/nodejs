0ac6f937510f147a773c2c96a06ca3236a27b1e8
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-01-15 14:36:25 -0300

    Criando script personalizado + node watch

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,7 @@
 import http  from "node:http"
 
 const server = http.createServer((request, response) => {
-  return response.end("Hello World!")
+  return response.end("Resposta do Servidor!!")
 })
 
 server.listen(3333)
