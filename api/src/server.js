b652a13f5c79f48bf7ec90cca59ecea90c7aeb94
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-01-20 14:20:31 -0300

    Utilizando middleware

--- api/src/server.js
+++ api/src/server.js
@@ -1,23 +1,18 @@
 import http from "node:http"
+import { jsonBodyHandler } from "./middlewares/jsonBodyHandler.js"
 
 const server = http.createServer(async (request, response) => {
   // object destructuring. É o mesmo que criar (const method = request.method)
-  const { method, url } = request 
+  const { method, url } = request
+
+  await jsonBodyHandler(request, response)
 
   if (method === "GET" && url === "/products") {
     return response.end("Lista de produtos!")
   }
 
   if (method === "POST" && url === "/products") {
-    const buffers = []
-
-    for await (const chunk of request) {
-      buffers.push(chunk)
-    }
-
-    console.log(Buffer.concat(buffers).toString())
-
-    return response.writeHead(201).end("Produto cadastrado!")
+    return response.writeHead(201).end(JSON.stringify(request.body))
   }
 
   return response.writeHead(404).end("Rota não encontrada!")
