1599627da2360b1348f456872aa104bf297ebbcb
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-01-20 09:57:24 -0300

    Utilizando Status Code

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,9 @@
 import http  from "node:http"
 
 const server = http.createServer((request, response) => {
-  return response.end("Resposta do Servidor!!")
+  // object destructuring. É o mesmo que criar (const method = request.method)
+  const { method } = request 
+  return response.writeHead(200).end("Criado com sucesso.")
 })
 
 server.listen(3333)
