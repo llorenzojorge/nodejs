aec490eb2ad0bb3d85a4cc994ea4c83702881d77
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-01-21 16:44:54 -0300

    Lendo dados no arquivo

--- api/src/database.js
+++ api/src/database.js
@@ -1,11 +1,13 @@
 import fs from "node:fs/promises"
-const DATABASE_PATH = new URL ("db.json", import.meta.url)
+const DATABASE_PATH = new URL("db.json", import.meta.url)
 
 export class Database {
   database = {}
 
   constructor() {
-    this.persist()
+    fs.readFile(DATABASE_PATH, "utf-8")
+      .then((data) => this.database = JSON.parse(data))
+      .catch(() => this.persist())
   }
 
   persist() {
